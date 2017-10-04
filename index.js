97f9c236199f4ac7edf9a4ada9a958db3db9a312
brendapalomino <32137996+brendapalomino@users.noreply.github.com>
2017-10-04 20:01:01 +0200

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    // only show CodePen link if on debug view (with no header)
-    if (location.pathname.indexOf('debug') > -1) {
-        $('#view-on-codepen').css('display','inline-table')
-    }
-});
